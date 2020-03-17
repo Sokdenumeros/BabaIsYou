e94f2ac816542ac729dc0e385131975b304f3548
Sergi <sergi.curto@est.fib.upc.edu>
2020-03-17 20:13:30 +0100

    .

--- BabaIsYou/ObjectMatrix.cpp
+++ BabaIsYou/ObjectMatrix.cpp
@@ -39,7 +39,6 @@ bool ObjectMatrix::recurs_players(int i, int j, movement m) {
 		matriu[current] = nullptr;
 		return true;
 	}
-	int z = 9;
 	if (matriu[next]->getsink()) {
 		delete matriu[next];
 		delete matriu[current];
@@ -69,138 +68,7 @@ bool ObjectMatrix::recurs_players(int i, int j, movement m) {
 	}
 	else return false;
 }
-/*
-bool ObjectMatrix::recurs_players_esquerra(int deltaTime, int i, int j) {
 
-	if (matriu[nc*i + j] == nullptr) return true;
-	else if ((matriu[nc*i + j]->itsname() == true && i > 0) || (matriu[nc*i + j]->getpush() == true && i > 0)) { //i - 1
-		if (recurs_players_esquerra(deltaTime, i - 1, j) == true) {
-			matriu[nc*(i-1)+j] = matriu[nc*(i) + j];
-			//matriu[nc*(i-1)+j]->setmove(false);
-			if (matriu[nc*(i - 1) + j]->getname() == "is1") {
-				is1x = i - 1;
-				is1y = j;
-			}
-			if (matriu[nc*(i - 1)+(j)]->getname() == "is2") {
-				is2x = i - 1;
-				is2y = j;
-			}
-			if (matriu[nc*(i - 1) + (j)]->getname() == "is3") {
-				is3x = i - 1;
-				is3y = j;
-			}
-			matriu[nc*(i) + j] = nullptr;
-			matriu[nc*(i-1)+j]->update(deltaTime, i - 1, j, true, false, true);
-			return true;
-		}
-	}
-	else if (matriu[nc*i + j]->getsink() == true) {
-		matriu[nc*(i + 1) + (j)]->update(deltaTime, 1000, j + 1, false, true, true);
-		matriu[nc*(i + 1) + j] = nullptr;
-		return false;
-	}
-	else return false;
-}
-
-bool ObjectMatrix::recurs_players_dreta(int deltaTime, int i, int j)
-{
-	if (matriu[nc*i + j] == nullptr) return true;
-	else if ((matriu[nc*i + j]->itsname() == true && i < 23) || (matriu[nc*i + j]->getpush() == true && i < 23)) { //i - 1
-		if (recurs_players_dreta(deltaTime, i + 1, j) == true) {
-			matriu[nc*(i + 1) + j] = matriu[nc*(i)+j];
-			//matriu[nc*(i-1)+j]->setmove(false);
-			if (matriu[nc*(i + 1) + j]->getname() == "is1") {
-				is1x = i + 1;
-				is1y = j;
-			}
-			if (matriu[nc*(i + 1)+(j)]->getname() == "is2") {
-				is2x = i + 1;
-				is2y = j;
-			}
-			if (matriu[nc*(i + 1) + (j)]->getname() == "is3") {
-				is3x = i + 1;
-				is3y = j;
-			}
-			matriu[nc*(i)+j] = nullptr;
-			matriu[nc*(i + 1) + j]->update(deltaTime, i + 1, j, true, true, true);
-			return true;
-		}
-	}
-	else if (matriu[nc*i + j]->getsink() == true) {
-		matriu[nc*(i - 1) + (j)]->update(deltaTime, 1000, j + 1, false, true, true);
-		matriu[nc*(i-1) + j] = nullptr;
-		
-		return false;
-	}
-	else return false;
-}
-
-bool ObjectMatrix::recurs_players_adalt(int deltaTime, int i, int j)
-{
-	if (matriu[nc*i + j] == nullptr) return true;
-	else if ((matriu[nc*i + j]->itsname() == true && j > 0) || (matriu[nc*i + j]->getpush() == true && j > 0)) { //i - 1
-		if (recurs_players_adalt(deltaTime, i, j-1) == true) {
-			matriu[nc*(i) + (j-1)] = matriu[nc*(i)+j];
-			//matriu[nc*(i-1)+j]->setmove(false);
-			if (matriu[nc*(i) + (j-1)]->getname() == "is1") {
-				is1x = i;
-				is1y = j - 1;
-			}
-			if (matriu[nc*(i)+(j - 1)]->getname() == "is2") {
-				is2x = i;
-				is2y = j - 1;
-			}
-			if (matriu[nc*(i)+(j - 1)]->getname() == "is3") {
-				is3x = i;
-				is3y = j - 1;
-			}
-			matriu[nc*(i)+j] = nullptr;
-			matriu[nc*(i) + (j-1)]->update(deltaTime, i, j - 1, false, false, true);
-			return true;
-		}
-	}
-	else if (matriu[nc*i + j]->getsink() == true) {
-		matriu[nc*(i)+(j + 1)]->update(deltaTime, 1000, j + 1, false, true, true);
-		matriu[nc*i + j + 1] = nullptr;
-		
-		return false;
-	}
-	else return false;
-}
-
-bool ObjectMatrix::recurs_players_abaix(int deltaTime, int i, int j)
-{
-	if (matriu[nc*i + j] == nullptr) return true;
-	else if ((matriu[nc*i + j]->itsname() == true && j < 23) || (matriu[nc*i + j]->getpush() == true && j < 23)) { //i - 1
-		if (recurs_players_abaix(deltaTime, i, j + 1) == true) {
-			matriu[nc*(i)+(j + 1)] = matriu[nc*(i)+j];
-			//matriu[nc*(i-1)+j]->setmove(false);
-			if (matriu[nc*(i)+(j + 1)]->getname() == "is1") {
-				is1x = i;
-				is1y = j + 1;
-			}
-			if (matriu[nc*(i)+(j + 1)]->getname() == "is2") {
-				is2x = i;
-				is2y = j + 1;
-			}
-			if (matriu[nc*(i)+(j + 1)]->getname() == "is3") {
-				is3x = i;
-				is3y = j + 1;
-			}
-			matriu[nc*(i)+j] = nullptr;
-			matriu[nc*(i)+(j + 1)]->update(deltaTime, i, j + 1, false, true, true);
-			return true;
-		}
-	}
-	else if (matriu[nc*i + j]->getsink() == true) {
-		matriu[nc*(i)+(j - 1)]->update(deltaTime, 1000, j + 1, false, true, true);
-		matriu[nc*i + j - 1] = nullptr;
-		
-		return false;
-	}
-	else return false;
-}
-*/
 void ObjectMatrix::search_is_dreta_esquerra(int varx, int vary)
 {
 	if (matriu[nc*(varx + 1) + vary] != nullptr && matriu[nc*(varx + 1) + vary]->itsname() == true) {
@@ -314,17 +182,12 @@ void ObjectMatrix::search_is_abaix_adalt(int varx, int vary)
 
 void ObjectMatrix::updat(int deltaTime)
 {
-	/*if (m[x][y].name == you) {
-		m[x][y + 1] = m[x][y];
-		m[x][y]->update(deltaTime, x, y);
-	}*/
 
-	for (int i = 0; i < 24; ++i) {
-		for (int j = 0; j < 24; ++j) {
+	for (int i = 0; i < nf; ++i) {
+		for (int j = 0; j < nc; ++j) {
 			if (matriu[nc*i + j] != nullptr) {
 				matriu[nc*i + j]->setsink(false);
 				matriu[nc*i + j]->setpush(false);
-				matriu[nc*i + j]->update(deltaTime,i,j);
 			}
 		}
 	}
@@ -343,51 +206,12 @@ void ObjectMatrix::updat(int deltaTime)
 	search_is_dreta_esquerra(is3x, is3y);
 	search_is_abaix_adalt(is3x, is3y);
 	search_is_adalt_abaix(is3x, is3y);
-	/*
-	movement m = NONE;
-	if (Game::instance().getSpecialKey(GLUT_KEY_LEFT))
-	{
-		Game::instance().specialKeyReleased(GLUT_KEY_LEFT);
-		m = LEFT;
-	}
-	if (Game::instance().getSpecialKey(GLUT_KEY_RIGHT))
-	{
-		Game::instance().specialKeyReleased(GLUT_KEY_RIGHT);
-		m = RIGHT;
-	}
-	if (Game::instance().getSpecialKey(GLUT_KEY_UP))
-	{
-		Game::instance().specialKeyReleased(GLUT_KEY_UP);
-		m = UP;
-	}
-	if (Game::instance().getSpecialKey(GLUT_KEY_DOWN))
-	{
-		Game::instance().specialKeyReleased(GLUT_KEY_DOWN);
-		m = DOWN;
-	}
-	if (m != NONE) {
-		for (int i = 0; i < 24; ++i) {
-			for (int j = 0; j < 24; ++j) {
-				if (matriu[nc*i + j] != nullptr && matriu[nc*i + j]->getmove()) {
 
-					if (i > 0) {
-						if (recurs_players_esquerra(deltaTime, i - 1, j) == true) {
-							matriu[nc*(i - 1) + j] = matriu[nc*i + j];
-							matriu[nc*(i - 1) + j]->setmove(false);
-							matriu[nc*i + j] = nullptr;
-							matriu[nc*(i - 1) + j]->update(deltaTime, i - 1, j, true, false, false);
-						}
-					}
-				}
-			}
-		}
-	}
-	*/
 	if (Game::instance().getSpecialKey(GLUT_KEY_LEFT))
 	{
 		Game::instance().specialKeyReleased(GLUT_KEY_LEFT);
-		for (int i = 0; i < 24; ++i) {
-			for (int j = 0; j < 24; ++j) {
+		for (int i = 0; i < nf; ++i) {
+			for (int j = 0; j < nc; ++j) {
 				if (matriu[nc*i + j] != nullptr && matriu[nc*i + j]->getmove()) recurs_players(i, j, LEFT);
 			}
 		}
@@ -396,8 +220,8 @@ void ObjectMatrix::updat(int deltaTime)
 	if (Game::instance().getSpecialKey(GLUT_KEY_RIGHT))
 	{
 		Game::instance().specialKeyReleased(GLUT_KEY_RIGHT);
-		for (int i = 23; i >= 0; --i) {
-			for (int j = 0; j < 24; ++j) {
+		for (int i = nf-1; i >= 0; --i) {
+			for (int j = 0; j < nc; ++j) {
 				if (matriu[nc*i + j] != nullptr && matriu[nc*i + j]->getmove()) recurs_players(i, j, RIGHT);
 			}
 		}
@@ -406,8 +230,8 @@ void ObjectMatrix::updat(int deltaTime)
 	if (Game::instance().getSpecialKey(GLUT_KEY_UP))
 	{
 		Game::instance().specialKeyReleased(GLUT_KEY_UP);
-		for (int i = 0; i < 24; ++i) {
-			for (int j = 0; j < 24; ++j) {
+		for (int i = 0; i < nf; ++i) {
+			for (int j = 0; j < nc; ++j) {
 				if (matriu[nc*i + j] != nullptr && matriu[nc*i + j]->getmove()) recurs_players(i, j, UP);
 			}
 		}
@@ -416,10 +240,15 @@ void ObjectMatrix::updat(int deltaTime)
 	if (Game::instance().getSpecialKey(GLUT_KEY_DOWN))
 	{
 		Game::instance().specialKeyReleased(GLUT_KEY_DOWN);
-		for (int i = 0; i < 24; ++i) {
-			for (int j = 23; j >= 0; --j) {
+		for (int i = 0; i < nf; ++i) {
+			for (int j = nc-1; j >= 0; --j) {
 				if (matriu[nc*i + j] != nullptr && matriu[nc*i + j]->getmove()) recurs_players(i, j, DOWN);
 			}
+		}
+	}
+	for (int i = 0; i < nf; ++i) {
+		for (int j = 0; j < nc; ++j) {
+			if (matriu[nc*i + j] != nullptr) matriu[nc*i + j]->update(deltaTime, i, j);
 		}
 	}
 }
@@ -468,31 +297,5 @@ void ObjectMatrix::rende()
 		}
 	}
 }
-
-/*bool ObjectMatrix::setPosition(int x, int y, Object* o) {
-	if (map[x*sizey + y] == nullptr) map[x*sizey + y] = o;
-	else return false;
-	return true;
-}
-
-void ObjectMatrix::setPos(Player p)
-{
-	matriu[p.getmapx()][p.getmapy()] = p;
-}
-
-Player[20][20] ObjectMatrix::getmat()
-{
-	return matriu;
-}
-
-void ObjectMatrix::setMat(vector<vector<Player>> m) {
-	matriu = m;
-}
-
-int ObjectMatrix::gettamany()
-{
-	return tamany;
-}
-*/
 
 //els is que tenen a dreta i esquerra
--- BabaIsYou/ObjectMatrix.h
+++ BabaIsYou/ObjectMatrix.h
@@ -15,10 +15,7 @@ private:
 	Player **matriu;
 	int nf, nc;
 	int is1x, is1y, is2x, is2y, is3x, is3y;
-	enum movement { NONE, UP, DOWN, RIGHT, LEFT };
-	//Object* map [];
-	//vector<Object> vec_obj;
-	//int tamany;
+	enum movement { UP, DOWN, RIGHT, LEFT };
 	bool recurs_players(int i, int j, movement m);
 
 public:
@@ -29,21 +26,10 @@ public:
 	void setPos(string nom, bool isname, float a, float b, int i, int j);
 	void setTileMa(TileMap *tileMap, int i, int j);
 	void rende();
-	//bool recurs_players_esquerra(int deltaTime, int i, int j);
-	//bool recurs_players_dreta(int deltaTime, int i, int j);
-	//bool recurs_players_adalt(int deltaTime, int i, int j);
-	//bool recurs_players_abaix(int deltaTime, int i, int j);
 	void search_is_dreta_esquerra(int varx, int vary);
 	void search_is_esquerra_dreta(int varx, int vary);
 	void search_is_adalt_abaix(int varx, int vary);
 	void search_is_abaix_adalt(int varx, int vary);
 
-	//bool setPosition(int x, int y, Object *o);
-	//vector<Object> getvec();
-	/*Player[20][20] getmat();
-	void setMat(vector<vector<Player>> m);
-	void setPos(Player p);
-	int gettamany();
-	*/
 
 };
